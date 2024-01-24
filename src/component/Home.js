fc73e16b977fcb4450f08b715f06a9891b829be5
Maliha <maliha.1601113@gmail.com>
2024-01-24 13:45:06 +0500

    frontend showing user data

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,39 +1,72 @@
-import React from "react";
+// Home.js
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
+import "./Home.css"; // Import the CSS file
 
 function Home() {
   const location = useLocation();
   const token = location.state.token;
-  console.log(token);
-  const api = `http://localhost:8080/users/getUser`;
-  axios.get(api, { headers: { "x-access-token": `${token}` } })
-    .then(res => {
-      console.log("Get User: ", res.data);
-      this.setState({
-        items: res.data,  /*set response data in items array*/
-        isLoaded: true,
-        redirectToReferrer: false
-      })
-    })
+  const [userData, setUserData] = useState(null);
+  const [allUsers, setAllUsers] = useState(null);
 
-    axios.get("http://localhost:8080/users/getAllUser")
-    .then(res => {
-      console.log("All User :", res.data);
-      this.setState({
-        items: res.data,  /*set response data in items array*/
-        isLoaded: true,
-        redirectToReferrer: false
-      })
-    })
-      return (
-        <div>
-          <Navbar />
-          <h3 className="pt-5 text-center">hello,i am Home Page</h3>
-          <h5>{token}</h5>
-        </div>
-      )
-    }
+  useEffect(() => {
+    // Fetch user data
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/users/getUser", {
+          headers: { "x-access-token": token }
+        });
+        setUserData(response.data);
+      } catch (error) {
+        console.error("Error fetching user data:", error.message);
+      }
+    };
+
+    // Fetch all users data
+    const fetchAllUsers = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/users/getAllUser");
+        setAllUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching all users data:", error.message);
+      }
+    };
+
+    // Call the functions
+    fetchUserData();
+    fetchAllUsers();
+  }, [token]);
+
+  return (
+    <div className="home-container">
+      <Navbar />
+      <h3 className="heading"></h3>
+
+      <div className="user-section">
+        <h4>User Data:</h4>
+        {userData ? (
+          <div className="user-data">
+            <pre>{JSON.stringify(userData, null, 2)}</pre>
+          </div>
+        ) : (
+          <p className="loading">Loading user data...</p>
+        )}
+      </div>
+
+      <div className="user-section">
+        <h4>All Users:</h4>
+        {allUsers ? (
+          <div className="all-users">
+            <pre>{JSON.stringify(allUsers, null, 2)}</pre>
+          </div>
+        ) : (
+          <p className="loading">Loading all users data...</p>
+        )}
+      </div>
+    </div>
+  );
+}
 
 export default Home;
